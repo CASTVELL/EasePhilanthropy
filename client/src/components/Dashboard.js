3343d9541ff6ad3077e5834bf0c16b57c6a9b204
CASTVELL <lmcastellanosa@correo.udistrital.edu.co>
2024-01-08 00:33:54 -0500

    added css styles to dashboard component

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import '../componentsCSS/Dashboard.css';
 
 function Dashboard() {
     const [charities, setCharities] = useState([]);
@@ -18,21 +19,33 @@ function Dashboard() {
     const filteredCharities = charities.filter(charity => charity.rate >= rateFilter[0] && charity.rate <= rateFilter[1]);
 
     return (
-        <div>
-            <input type="range" min="0" max="5" value={rateFilter[0]} onChange={e => setRateFilter([e.target.value, rateFilter[1]])} />
-            {/*<input type="range" min="0" max="5" value={rateFilter[1]} onChange={e => setRateFilter([rateFilter[0], e.target.value])} />*/}
-            <table>
-                <tbody>
-                    {filteredCharities.map(charity => (
-                        <tr key={charity.id}>
-                            <td>{charity.name}</td>
-                            <td>{charity.description}</td>
-                            <td>{charity.rate}</td>
-                            <td><Link to={`/charity/${charity.id}`}>Details</Link></td>
+        <div className="dashboard">
+            <div className="filter-section">
+                <label htmlFor="rate">Rate   ({rateFilter[0]})</label>
+                <input id="rate" type="range" min="0" max="5" value={rateFilter[0]} onChange={e => setRateFilter([e.target.value, rateFilter[1]])} />
+            </div>
+            <div className="charities-section">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Rate</th>
+                            <th>LINK</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {filteredCharities.map(charity => (
+                            <tr key={charity.id}>
+                                <td>{charity.name}</td>
+                                <td>{charity.description}</td>
+                                <td>{charity.rate}</td>
+                                <td><Link to={`/charity/${charity.id}`}>Details</Link></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
